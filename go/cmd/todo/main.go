95a70ab6580ff5afc8195e48e3f68ae4efdb4af1
yanicksenn <ys+github@yanicksenn.com>
2025-07-17 20:20:21 +0200

    feat(todo): implement recursive file traversal

--- go/cmd/todo/main.go
+++ go/cmd/todo/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("TODO tool coming soon!")
+	// By default, search the current directory. This can be overridden by a command-line argument.
+	searchDir := "."
+	flag.Parse()
+	if flag.NArg() > 0 {
+		searchDir = flag.Arg(0)
+	}
+
+	// Convert to an absolute path for cleaner output.
+	absPath, err := filepath.Abs(searchDir)
+	if err != nil {
+		log.Fatalf("Error getting absolute path for %q: %v", searchDir, err)
+	}
+
+	fmt.Printf("Searching for TODOs in: %s\n", absPath)
+
+	err = filepath.WalkDir(absPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			// If we can't access a path, report it and skip it.
+			fmt.Fprintf(os.Stderr, "Error accessing path %q: %v\n", path, err)
+			return nil
+		}
+
+		// Skip directories.
+		if d.IsDir() {
+			return nil
+		}
+
+		// For now, just print the file path.
+		// Later, we will parse the file for TODOs here.
+		fmt.Println(path)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Error walking directory %q: %v", absPath, err)
+	}
 }
