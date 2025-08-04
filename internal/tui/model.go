d3e36b1193e9d41c69638e40422ec12fbc085abf
Yanick Senn <ys+github@yanicksenn.com>
2025-08-04 17:23:30 +0200

    feat(tui): show completed tasks

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -128,7 +128,7 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.SetStatus("Task deleted")
 		selectedTaskList := m.lists[TaskListsPane].SelectedItem().(taskListItem)
 		return m, func() tea.Msg {
-			tasks, err := m.client.ListTasks(gtasks.ListTasksOptions{TaskListID: selectedTaskList.Id})
+			tasks, err := m.client.ListTasks(gtasks.ListTasksOptions{TaskListID: selectedTaskList.Id, ShowCompleted: true})
 			if err != nil {
 				return errorMsg{err}
 			}
@@ -139,7 +139,7 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.SetStatus("Task completed")
 		selectedTaskList := m.lists[TaskListsPane].SelectedItem().(taskListItem)
 		return m, func() tea.Msg {
-			tasks, err := m.client.ListTasks(gtasks.ListTasksOptions{TaskListID: selectedTaskList.Id})
+			tasks, err := m.client.ListTasks(gtasks.ListTasksOptions{TaskListID: selectedTaskList.Id, ShowCompleted: true})
 			if err != nil {
 				return errorMsg{err}
 			}
